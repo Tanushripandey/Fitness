25f15de2efceac165ca97797b66ef7fd5eaf2c69
Tanushripandey <tanushri.it@gmail.com>
2020-04-03 00:34:58 +0530

    page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,30 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then( m => m.PagesPageModule)
+  },
+  {
+    path: 'starter',
+    loadChildren: () => import('./page/starter/starter.module').then( m => m.StarterPageModule)
+  },
+  {
+    path: 'starter',
+    loadChildren: () => import('./starter/starter.module').then( m => m.StarterPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'plans',
+    loadChildren: () => import('./plans/plans.module').then( m => m.PlansPageModule)
   }
 ];
 @NgModule({
