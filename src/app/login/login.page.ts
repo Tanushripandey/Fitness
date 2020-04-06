b17143368babff09ec7014805203e2c90674f7cc
Tanushripandey <tanushri.it@gmail.com>
2020-04-06 16:29:29 +0530

    some changes

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController, AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,13 +9,97 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  constructor(public router: Router) { }
-
-  ngOnInit() {
-  }
+   //When we define public router: Router, Angular will create an object for Router class.
+   loginForm: FormGroup;
+   errorMessage: string;
+ 
+ 
+   validationMessages = {
+   
+     email: [
+       {
+         type: 'required',
+         message: ('Email field can`t blank')
+       },
+       {
+         type: 'pattern',
+         message: ('Please enter valid email.')
+       }
+     ],
+    
+     password: [
+       {
+         type: 'required',
+         message: ('password field can`t blank')
+       },
+       {
+         type: 'minLength',
+         message: ('Password should be min in 6 words.')
+       },
+       {
+         type: 'maxLength',
+         message: ('Password should be not more than in 12 words.')
+       },
+       {
+         type: 'pattern',
+         message: ('Your password must start with one uppercase, one lowercase, and one number')
+       }
+     ],
+  
+   };
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   //constructor(public router: Router) { }
+ 
+   //ngOnInit() {
+     // let userData = JSON.parse(localStorage.getItem("userData"));
+     // let email = userData.email;
+     // this.email = email;
+   //}
+   constructor(private fb: FormBuilder, public router: Router, public toastController: ToastController, public alertController: AlertController) { 
+     this.loginForm = this.fb.group({
+     
+       email: [
+         '',
+         [
+           Validators.required,
+           Validators.email,
+           Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')
+         ]
+       ],
+     
+       password: ['', [
+         Validators.required,
+         Validators.minLength(6),
+         Validators.maxLength(12), 
+         Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+       ]],
+       
+     },
+     // {validator: MustMatch('password', 'confirmPassword')}
+     );
+   }
+ 
+   ngOnInit() {
+   }
+ 
+   async presentToast() {
+     const toast = await this.toastController.create({
+       message: 'Logged Successfully.',
+       color: 'danger',
+       duration: 2000
+     });
+     toast.present();
+   }
+   
 //Login Method for Routing to home from login
 login(){
- 
+  this.presentToast();
   this.router.navigate(['/tabs/tab1']);
 }
 //Register Method for Routing to login from register
@@ -25,3 +111,4 @@ forgot_password(){
 this.router.navigate(['/forgot-password']);
 }
 }
+
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,125 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  errorMessage: string;
+  registerData: any;
+
+  validationMessages = {
+    userName: [
+      {
+        type: 'required',
+        message: ('Name field can`t blank')
+      }
+    ],
+    email: [
+      {
+        type: 'required',
+        message: ('Email field can`t blank')
+      },
+      {
+        type: 'pattern',
+        message: ('Please enter valid email.')
+      }
+    ],
+    contact: [
+      {
+        type: 'required',
+        message: ('Contact field can`t blank')
+      },
+      {
+        type: 'pattern',
+        message: ('Please enter valid mobile number.')
+      }
+    ],
+    password: [
+      {
+        type: 'required',
+        message: ('password field can`t blank')
+      },
+      {
+        type: 'minLength',
+        message: ('Password should be min in 6 words.')
+      },
+      {
+        type: 'maxLength',
+        message: ('Password should be not more than in 12 words.')
+      },
+      {
+        type: 'pattern',
+        message: ('Your password must start with one uppercase, one lowercase, and one number')
+      }
+    ],
+    confirmPassword: [
+      {
+        type: 'required',
+        message: ('confirmPassword field can`t blank')
+      },
+      {
+        type: 'minLength',
+        message: ('Password should be min in 6 words.')
+      },
+      {
+        type: 'maxLength',
+        message: ('Password should be not more than in 12 words.')
+      },
+      {
+        type: 'pattern',
+        message: ('Please enter valid password')
+      }
+    ]
+  };
+
+  constructor(private fb: FormBuilder, public router: Router, public toastController: ToastController) { 
+    this.registerForm = this.fb.group({
+      userName: ['',[Validators.required]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')
+        ]
+      ],
+      contact: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")
+        ]
+      ],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12), 
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ]],
+      confirmPassword:  ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12), 
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]]
+    },
+    // {validator: MustMatch('password', 'confirmPassword')}
+    );
+  }
 
   ngOnInit() {
   }
 
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'You have created your account successfully.',
+      color: 'danger',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+
+  submit(){
+    this.presentToast();
+      this.router.navigate(['/login']);
+    }
+    
 }
